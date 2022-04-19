e021a877a5c699f0c50b2ff8dd45c5105980a5bb
saidugurlu <ugurlumsaid@gmx.de>
2022-04-19 13:28:12 +0200

    jsaon created and finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.scss';
+import "./App.scss";
+import data from "./data/data.json";
 
 function App() {
   return (
     <div className="App">
- test
+      <h1>App</h1>
+      <img src="./images/ruegen.jpg" alt="" />
+      <div>
+        First employee: {data.employees[0].firstName}{" "}
+        {data.employees[0].lastName}
+      </div>
+      <div>
+        Second employee: {data.employees[1].firstName}{" "}
+        {data.employees[1].lastName}
+      </div>
     </div>
   );
 }
-
 export default App;
